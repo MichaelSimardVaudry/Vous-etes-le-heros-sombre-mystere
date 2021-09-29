1b093e19c45c8314fc5464ac4a2587e61767823b
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2021-09-29 17:51:02 -0400

    finChapter

--- simard_michael_tp01/script.js
+++ simard_michael_tp01/script.js
@@ -4,16 +4,88 @@ let chaptersObj = {
 
 let chapter1 = {
   subtitle: "pris au piège",
-  text: "Touts d'un coups vous sentez de forte vibration de nul part, d'un seul coups vous vous eppercevezq eu les mure de chaque coter de la piece bouge ne ce refermant sur vous",
+  text: "Touts d'un coups vous sentez de forte vibration de nul part, d'un seul coups vous vous apercevez que les mure de chaque coter de la piece bouge ne ce refermant sur vous",
   img: "simard_michael_tp01/assets/img/chapter_01_wall.jpg",
+};
+let chapter1_1 = {
+  subtitle: "pris au piège",
+  text: "vous arrivez dans une salle  avec une petite statue qui vous regade et vous dits félicitaion !",
+  img: "simard_michael_tp01/assets/img/chapter_01_01_statue.jpg",
+};
+let chapter1_2 = {
+  subtitle: "pris au piège",
+  text: "Vous franchiser une petite porte.",
+  img: "simard_michael_tp01/assets/img/chapter_01_02_porte.jpg",
 };
 let chapter2 = {
   subtitle: "vous ou elle",
-  text: "En entrant dans la piece vous appercevez un gros tube avec une inconnu a l'interieur. Sur un panneau devant vous ce trouve 3 bouton, un rouge, un bleu et un vert.",
+  text: "En entrant dans la piece vous appercevez un gros tube avec une inconnue a l'interieur. Sur un panneau devant vous, ce trouve 3 boutons, un rouge, un bleu et un vert.",
   img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+};
+let chapter2_1 = {
+  subtitle: "vous ou elle",
+  text: "la piece et le tube se rempli de poison",
+  img: "simard_michael_tp01/assets/img/chapter_02_01_gas.png",
+};
+let chapter2_2_1 = {
+  subtitle: "vous ou elle",
+  text: "Le tube devant vous ce remplit de gas",
+  img: "simard_michael_tp01/assets/img/chapter_02_01_gas.png",
+};
+let chapter2_2_2 = {
+  subtitle: "vous ou elle",
+  text: "la porte devant vous s'ouvre, vous la franchisez.",
+  img: "simard_michael_tp01/assets/img/chapter_01_02_porte.jpg",
+};
+let chapter2_3 = {
+  subtitle: "vous ou elle",
+  text: "La piece ce rempli de poison.",
+  img: "simard_michael_tp01/assets/img/chapter_02_01_gas.png",
 };
 let chapter3 = {
-  subtitle: "vous ou elle",
-  text: "En entrant dans la piece vous appercevez un gros tube avec une inconnu a l'interieur. Sur un panneau devant vous ce trouve 3 bouton, un rouge, un bleu et un vert.",
-  img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+  subtitle: "Vitesse",
+  text: "Devant vous sur la table ce trouve une arme. celle ci est chargé d'une balle. vous comprenez que vous devrez jouer a la roulette russe.",
+  img: "simard_michael_tp01/assets/img/chapter_03_pistolet.jpg",
 };
+let chapter3_1 = {
+  subtitle: "Vitesse",
+  text: "vous tirez une balle dans votre tête.",
+  img: "simard_michael_tp01/assets/img/chapter_03_pistolet.jpg",
+};
+let chapter3_2 = {
+  subtitle: "Vitesse",
+  text: "L'arme ne tire pas.",
+  img: "simard_michael_tp01/assets/img/chapter_03_pistolet.jpg",
+};
+let chapter4 = {
+  subtitle: "Repas joyeux",
+  text: "Sur une une table devant vous, ce trooube 3 puddings, vous devez faire le cchoix dent manger un.",
+  img: "simard_michael_tp01/assets/img/chapter_04_table.jpg",
+};
+let chapter4_1 = {
+  subtitle: "Repas joyeux",
+  text: "Vous êtes drogé. vous commencer a sentire des effets étranges. votre âme sort de votre corps.",
+  img: "simard_michael_tp01/assets/img/chapter_04_01.png",
+};
+let chapter4_1_1 = {
+  subtitle: "Repas joyeux",
+  text: "Vous decidez de trverser les murs.",
+  img: "simard_michael_tp01/assets/img/chapter_04_01.png",
+};
+let chapter4_1_2 = {
+  subtitle: "Repas joyeux",
+  text: "vous vous trouvez dans une piece munis dun mimoir a travers lequelle vous pouvez voir.",
+  img: "simard_michael_tp01/assets/img/chapter_04_01.png",
+};
+let chapter4_1_3 = {
+  subtitle: "Repas joyeux",
+  text: "Vous apercevez la piece originel ou vous vous ètes reveillez.",
+  img: "simard_michael_tp01/assets/img/chapter_04_01.png",
+};
+let chapter4_1_4 = {
+  subtitle: "Repas joyeux",
+  text: "vous mourez d'une overdose de stupéfiant.",
+  img: "simard_michael_tp01/assets/img/chapter_04_01.png",
+};
+
+function goToChapter(chapterName) {}
