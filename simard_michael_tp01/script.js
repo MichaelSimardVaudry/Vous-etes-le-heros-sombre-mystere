c7784c9dfc1f58513b7067776b5ae21433485e8f
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2021-09-29 18:38:55 -0400

    tp02

--- simard_michael_tp01/script.js
+++ simard_michael_tp01/script.js
@@ -1,11 +1,44 @@
 let chaptersObj = {
-  slug: "le_jeux",
+  le_piege: 
+  let chapter1 = {
+    subtitle: "pris au piège",
+    text: "Touts d'un coups vous sentez de forte vibration de nul part, d'un seul coups vous vous apercevez que les mure de chaque coter de la piece bouge ne ce refermant sur vous",
+    img: "simard_michael_tp01/assets/img/chapter_01_wall.jpg",
+    options: function chp01Arr() = ["vous briser le miroir 'seulement plus tard')", "vous grimpez sur le dessu du mur ", "mettre un morceau de fer pour bloquer les murs"];,
+  };,
+
+  
+
+  le_gas:
+  let chapter2 = {
+    subtitle: "vous ou elle",
+    text: "En entrant dans la piece vous appercevez un gros tube avec une inconnue a l'interieur. Sur un panneau devant vous, ce trouve 3 boutons, un rouge, un bleu et un vert.",
+    img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+    options: function chp01Arr() = ["appuyer sur le bouton noir')", "appuyer sur le bouton Bleu ", "appuyer sur le bouton Rouge"];,
+  };,
+
+  a_vitesse:
+  let chapter3 = {
+    subtitle: "Vitesse",
+    text: "Devant vous sur la table ce trouve une arme. celle ci est chargé d'une balle. vous comprenez que vous devrez jouer a la roulette russe.",
+    img: "simard_michael_tp01/assets/img/chapter_03_pistolet.jpg",
+    options: function chp01Arr() = ["vous tournez rapidement", "vous tournez lentement"];,
+  };,
+  
+  le_repas:
+  let chapter4 = {
+    subtitle: "Repas joyeux",
+    text: "Sur une une table devant vous, ce trooube 3 puddings, vous devez faire le cchoix dent manger un.",
+    img: "simard_michael_tp01/assets/img/chapter_04_table.jpg",
+    options: function chp01Arr() = ["puding droit", "puding du centre","puding gauche"];,
+  };,
 };
 
 let chapter1 = {
   subtitle: "pris au piège",
   text: "Touts d'un coups vous sentez de forte vibration de nul part, d'un seul coups vous vous apercevez que les mure de chaque coter de la piece bouge ne ce refermant sur vous",
   img: "simard_michael_tp01/assets/img/chapter_01_wall.jpg",
+  options: function chp01Arr() = ["vous briser le miroir 'seulement plus tard')", "vous grimpez sur le dessu du mur ", "mettre un morceau de fer pour bloquer les murs"];,
 };
 let chapter1_1 = {
   subtitle: "pris au piège",
@@ -21,6 +54,7 @@ let chapter2 = {
   subtitle: "vous ou elle",
   text: "En entrant dans la piece vous appercevez un gros tube avec une inconnue a l'interieur. Sur un panneau devant vous, ce trouve 3 boutons, un rouge, un bleu et un vert.",
   img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+  options: function chp01Arr() = ["appuyer sur le bouton noir')", "appuyer sur le bouton Bleu ", "appuyer sur le bouton Rouge"];,
 };
 let chapter2_1 = {
   subtitle: "vous ou elle",
@@ -46,6 +80,7 @@ let chapter3 = {
   subtitle: "Vitesse",
   text: "Devant vous sur la table ce trouve une arme. celle ci est chargé d'une balle. vous comprenez que vous devrez jouer a la roulette russe.",
   img: "simard_michael_tp01/assets/img/chapter_03_pistolet.jpg",
+  options: function chp01Arr() = ["vous tournez rapidement", "vous tournez lentement"];,
 };
 let chapter3_1 = {
   subtitle: "Vitesse",
@@ -61,6 +96,7 @@ let chapter4 = {
   subtitle: "Repas joyeux",
   text: "Sur une une table devant vous, ce trooube 3 puddings, vous devez faire le cchoix dent manger un.",
   img: "simard_michael_tp01/assets/img/chapter_04_table.jpg",
+  options: function chp01Arr() = ["puding droit", "puding du centre","puding gauche"];,
 };
 let chapter4_1 = {
   subtitle: "Repas joyeux",
