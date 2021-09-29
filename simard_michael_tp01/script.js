92e5c44327c5beefd50b2ad5b17715c2c56353e8
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2021-09-29 11:38:29 -0400

    chapter

---
+++ simard_michael_tp01/script.js
@@ -0,0 +1,19 @@
+let chaptersObj = {
+  slug: "le_jeux",
+};
+
+let chapter1 = {
+  subtitle: "pris au piège",
+  text: "Touts d'un coups vous sentez de forte vibration de nul part, d'un seul coups vous vous eppercevezq eu les mure de chaque coter de la piece bouge ne ce refermant sur vous",
+  img: "simard_michael_tp01/assets/img/chapter_01_wall.jpg",
+};
+let chapter2 = {
+  subtitle: "vous ou elle",
+  text: "En entrant dans la piece vous appercevez un gros tube avec une inconnu a l'interieur. Sur un panneau devant vous ce trouve 3 bouton, un rouge, un bleu et un vert.",
+  img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+};
+let chapter3 = {
+  subtitle: "vous ou elle",
+  text: "En entrant dans la piece vous appercevez un gros tube avec une inconnu a l'interieur. Sur un panneau devant vous ce trouve 3 bouton, un rouge, un bleu et un vert.",
+  img: "simard_michael_tp01/assets/img/chapter_02_tube.png",
+};
