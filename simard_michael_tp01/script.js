b33250137a6302481c08539866adbd3ebf16356e
smnarnold <moi@smnarnold.com>
2021-10-24 22:55:12 -0400

    fixes

--- simard_michael_tp01/script.js
+++ simard_michael_tp01/script.js
@@ -1,15 +1,17 @@
 let keyFounded = false;
-(pieceSecretKey = function () {
-  keyFounded++;
+
+function pieceSecretKey() {
+  keyFounded = true;
   goToChapter(`chapter1`);
-}),
-  (pieceSecrete = function () {
-    if (keyFounded == true) {
-      goToChapter(`chapter0_2`);
-    } else {
-      goToChapter(`chapter0`);
-    }
-  });
+};
+
+function pieceSecrete() {
+  if (keyFounded == true) {
+    goToChapter(`chapter0_2`);
+  } else {
+    goToChapter(`chapter0`);
+  }
+};
 
 let chaptersObj = {
   chapter1: {
@@ -298,8 +300,6 @@ let chaptersObj = {
   },
 };
 
-console.log(chaptersObj.chapter1.subtitle);
-
 function goToChapter(chapterName) {
   let chapter = chaptersObj[chapterName];
 
@@ -313,11 +313,13 @@ function goToChapter(chapterName) {
   descriptionChap.innerHTML = chapter.text;
   titre.innerHTML = chapter.subtitle;
 
-  for (let index = 0; index < choixArr.lengt; index++) {
+  for (let index = 0; index < choixArr.length; index++) {
     const opt = choixArr[index];
-    choixBut += `<input class="boutons" type="submit" value="${opt.text}" onclick="${opt.action}">`;
+    choixBut += `<button onclick="${opt.action}">${opt.text}</button>`;
     console.log(choixBut);
   }
   let choixBar = document.querySelector(".boutons");
   choixBar.innerHTML = choixBut;
 }
+
+goToChapter('chapter1');
